4a597cb7c4ecd95f84f6b2dfc4000cc804e22ad5
Per Orest <per.orest@outlook.com>
2019-12-15 23:26:20 +0100

    Add project files.

---
+++ Models/SettingModels/LocationType.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PRORegister.Models.SettingModels
+{
+    public class LocationType
+    {
+    }
+}
---
+++ Models/ViewModels/SettingsViewModel.cs
@@ -0,0 +1,27 @@
+﻿using PRORegister.Models.SettingModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PRORegister.Models.ViewModels
+{
+    public class SettingsViewModel
+    {
+        public List<AssociationType> AssociationTypes { get; internal set; }
+
+        public List<CommunicationType> CommunicationTypes { get; internal set; }
+
+        public List<EconomicsType> EconomicsTypes { get; internal set; }
+
+        public List<LocationType> LocationTypes { get; internal set; }
+
+        public List<PersonRole> PersonRoles { get; internal set; }
+
+        public List<PersonStatus> PersonStatuses { get; internal set; }
+
+        public List<PersonType> PersonTypes { get; internal set; }
+
+
+    }
+}
